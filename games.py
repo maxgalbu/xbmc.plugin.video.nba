e0b1ba8cdafad57f1bec956fc7fb9faf51497f72
Massimo Galbusera <max.galbusera@gmail.com>
2015-11-05 00:48:48 +0100

    try the target bitrate, and if it doesn't exist try every bitrate lower than the target bitrate

--- games.py
+++ games.py
@@ -117,15 +117,17 @@ def getHighlightGameUrl(video_id):
 
 def getGameUrlWithBitrate(url, video_type):
     # Force the bitrate by modifying the HLS url and adding the bitrate
-    available_bitrates = {
-        72060: 4500,
-        720: 3000,
-        540: 1600,
-        432: 1200,
-        360: 800,
-    }
-    target_bitrate = available_bitrates.get(vars.target_video_height, 1600)
-    failsafe_bitrate = available_bitrates.get(360)
+    available_bitrates = [
+        (72060, 4500),
+        (720, 3000),
+        (540, 1600),
+        (432, 1200),
+        (360, 800),
+    ]
+
+    #Get the chosen bitrate, and all the bitrates after that one
+    target_bitrates = [item[1] for item in available_bitrates if item[0] <= vars.target_video_height]
+    log('target_bitrates: %s' % target_bitrates, xbmc.LOGDEBUG)
 
     regex_pattern = 'whole_([0-9])_ipad'
     regex_replacement_format = r'whole_\1_%s_ipad'
@@ -136,16 +138,16 @@ def getGameUrlWithBitrate(url, video_type):
         regex_pattern = '([a-z]+)_hd_ipad'
         regex_replacement_format = r'\1_hd_%s_ipad'
 
-    #Try the target bitrate
-    selected_video_url = re.sub(regex_pattern, regex_replacement_format % target_bitrate, url)
-    if urllib.urlopen(selected_video_url).getcode() != 200:
-        log("video of height %d not found, trying with height 360" % vars.target_video_height, xbmc.LOGDEBUG)
+    #Try the target bitrate, and if it doesn't exists, try every bitrate lower than the chosen one
+    #(eg: if you chouse 3000k as bitrate, it will try 3000k, then 1600k, then 1200k, 
+    #and so on)
+    selected_video_url = ""
+    for target_bitrate in target_bitrates:
+        selected_video_url = re.sub(regex_pattern, regex_replacement_format % target_bitrate, url)
+        if urllib.urlopen(selected_video_url).getcode() == 200:
+            break
 
-        #Try the failsafe url (bitrate of 800)
-        selected_video_url = re.sub(regex_pattern, regex_replacement_format % failsafe_bitrate, url)
-        if urllib.urlopen(selected_video_url).getcode() != 200:
-            log("failsafe bitrate video not found, bailing out", xbmc.LOGDEBUG)
-            selected_video_url = ""
+        log("video of bitrate %d not found, trying with next height" % target_bitrate, xbmc.LOGDEBUG)
 
     return selected_video_url
 
